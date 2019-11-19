00ba6b0d92eeec26c1a05667e741709f9ea2da6a
Randy Taylor <randyisbusy@gmail.com>
2019-11-19 17:30:12 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,10 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
-  end
+  end 
+  
+  def show
+    id = params["id"]
+   @student = Student.find(id)
+  end  
 end
